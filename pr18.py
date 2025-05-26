083386435eb80cc7bf44c52dc31a7e4246e8a579
Hemangi Patel <162620782+Hemangi-patel109@users.noreply.github.com>
2025-05-26 10:39:21 +0530

    Add files via upload

---
+++ pr18.py
@@ -0,0 +1,12 @@
+#hogward.py
+
+students = ["Hermione","Harry","Ron"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+#Or line 5,6,7 can be written as:
+
+for students in students:
+    print(students)
