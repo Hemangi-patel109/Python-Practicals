6df9bca4f204533b46afebd4919cc650160e40f7
Hemangi Patel <162620782+Hemangi-patel109@users.noreply.github.com>
2025-05-27 15:03:31 +0530

    Add files via upload

---
+++ pr20.py
@@ -0,0 +1,11 @@
+#dictionaries:
+
+students = {
+    "Hermione" : "Geyffindor",
+    "Harry" : "Geyffindor",
+    "Ron" : "Geyffindor",
+    "Draco" : "Slytherin",
+}
+
+for student in students:
+    print(student , students[student] , sep=" , ")
