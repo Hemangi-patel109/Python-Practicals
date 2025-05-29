d32d2585b08f6754bc4321bb773c981c77b57560
Hemangi Patel <162620782+Hemangi-patel109@users.noreply.github.com>
2025-05-29 09:14:15 +0530

    Add files via upload

---
+++ pr24.py
@@ -0,0 +1,13 @@
+#o/p: #
+#     ##
+#     ### & so on...
+
+def main():
+    height = int(input("Height: "))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print("#"*(i+1))
+
+main()
